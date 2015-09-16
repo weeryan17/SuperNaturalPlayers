f1d25e2c12b6df42aa6d458874e1a0c9e6bfe775
weeryan17 <rmohle17@bhmschools.org>
2015-09-16 17:27:33 -0500

    wrapped up a few bugs with the zombie horse.

--- SuperNaturalPlayers/src/com/weeryan17/snp/Commands/MobCommand.java
+++ SuperNaturalPlayers/src/com/weeryan17/snp/Commands/MobCommand.java
@@ -1,21 +1,28 @@
 package com.weeryan17.snp.Commands;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.md_5.bungee.api.ChatColor;
 import net.minecraft.server.v1_8_R3.EntityLiving;
 import net.minecraft.server.v1_8_R3.GenericAttributes;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
 import org.bukkit.entity.CaveSpider;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Zombie;
+import org.bukkit.inventory.HorseInventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -23,7 +30,7 @@ import com.weeryan17.snp.Main;
 
 public class MobCommand implements CommandExecutor {
     private Main instance;
-    
+    Map<Entity, Integer> map = new HashMap<Entity, Integer>();
     public MobCommand(Main instance) {
         this.instance = instance;
     }
@@ -75,16 +82,36 @@ public class MobCommand implements CommandExecutor {
     					} else if(args[0].equals("ZombieHorse") || args[0].equals("zombiehorse")){
     						if(this.instance.getConfig().getInt("Players." + player + ".Souls") >= 100){
     							int souls = this.instance.getConfig().getInt("Players." + player + ".Souls");
-    							Player pl = Bukkit.getPlayer(player);
-    							Horse horse = (Horse)loc.getWorld().spawnEntity(loc, EntityType.HORSE);
+    							final Player pl = Bukkit.getPlayer(player);
+    							final Horse horse = (Horse)loc.getWorld().spawnEntity(loc, EntityType.HORSE);
     							horse.setAdult();
-    							horse.setHealth(15);
+    							horse.setHealth(10);
     							horse.setTamed(true);
     							horse.setCarryingChest(true);
     							horse.setVariant(Horse.Variant.UNDEAD_HORSE);
-    							horse.setJumpStrength(10);
+    							horse.setJumpStrength(1);
     							horse.setPassenger(pl);
-    							((EntityLiving)((CraftEntity)horse).getHandle()).getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).setValue(10);
+    							int stop = Bukkit.getScheduler().scheduleSyncRepeatingTask(instance, new Runnable(){
+
+									@Override
+									public void run() {
+										if(horse.getPassenger() != pl){
+											horse.remove();
+											Bukkit.getScheduler().cancelTask(map.get(horse));
+											pl.sendMessage(ChatColor.DARK_GRAY + "You got off you're horse so it died");
+										}
+										if(horse.isDead()){
+											Bukkit.getScheduler().cancelTask(map.get(horse));
+											pl.sendMessage(ChatColor.DARK_GRAY + "You're horse died");
+										}
+									}
+    								
+    							}, 1, 0);
+    							map.put(horse, stop);
+    							HorseInventory inv = horse.getInventory();
+    							ItemStack item = new ItemStack(Material.SADDLE);
+    							inv.addItem(item);
+    							((EntityLiving)((CraftEntity)horse).getHandle()).getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).setValue(1);
     							sender.sendMessage(ChatColor.DARK_BLUE + "You summon a zombie horse for 100 souls");
     							this.instance.getConfig().set("Players." + player + ".Souls", souls - 100);
     						} else {
